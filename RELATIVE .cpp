bb4128d105a8ed311c89f29d9637fb2f6129ecde
Nikhil <nikhilraj875727@gmail.com>
2021-09-21 11:55:16 +0530

    New problem added

---
+++ RELATIVE .cpp
@@ -0,0 +1,16 @@
+//July Challenge 2021
+//RELATIVE 
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int T, g, c;
+    cin >> T;
+    
+    while(T--){
+       cin >> g >> c;
+       cout << (c*c)/(2*g) <<endl;
+    }
+}
