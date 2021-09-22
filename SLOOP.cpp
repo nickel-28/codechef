0ffe08c810fbdfe4cc9c03e4302a3b7cfcf279e8
Nikhil <nikhilraj875727@gmail.com>
2021-09-22 11:53:39 +0530

    added new problem

---
+++ SLOOP.cpp
@@ -0,0 +1,17 @@
+//SLOOP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int M,S;
+	    cin>>M>>S;
+	    int T=M/S;
+	    cout<<T<<endl;
+	}
+	return 0;
+}
