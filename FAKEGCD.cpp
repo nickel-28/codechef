21bf1a8a8b971d0f6640d958d9851327a1a39310
nickel-28 <nikhilraj875727@gmail.com>
2021-10-12 15:54:43 +0530

    Problem Added

---
+++ FAKEGCD.cpp
@@ -0,0 +1,37 @@
+// FAKEGCD
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// void solve(){
+//     int n;
+//     cin>>n;
+    
+//     int gcd(vector<int> vec, int n)
+//     {
+//         int gcd = vec[0];
+//         for (int i = 1; i < n; i++)
+//         {
+//             gcd = __gcd(gcd, vec[i]);
+//         }
+//         return gcd;
+//     }
+    
+// }
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,sum=1,i;
+        cin>>n;
+        
+            for(i=1; i<=n; i++){
+                
+                cout<<i<<" ";
+            }
+        
+	}
+	return 0;
+}
