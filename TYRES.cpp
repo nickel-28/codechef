ea256064fbe612a3050f7915bb370a8c2011addb
nickel-28 <nikhilraj875727@gmail.com>
2021-10-12 15:52:20 +0530

    Problem Added

---
+++ TYRES.cpp
@@ -0,0 +1,26 @@
+//TYRES
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        if(n%4==0){
+            cout<<"NO"<<endl;
+        }
+        else if(n%4!=0) cout<<"YES"<<endl;
+    }
+    else cout <<"NO"<<endl;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
