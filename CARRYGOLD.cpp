246c4f33f31b25211fa9aa8a0909d1eee24786a5
nickel-28 <nikhilraj875727@gmail.com>
2021-10-16 11:13:10 +0530

    New Problem

---
+++ CARRYGOLD.cpp
@@ -0,0 +1,22 @@
+//CARRYGOLD
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    if(y*(n+1)>=x) cout << "YES" << endl;
+    else cout << "NO" <<endl;
+    
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
