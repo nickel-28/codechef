356d45abe38eb041e00099ef7714c110eaaa2ed9
nickel-28 <nikhilraj875727@gmail.com>
2021-10-16 11:21:34 +0530

    New problem

---
+++ CHFRICH.cpp
@@ -0,0 +1,15 @@
+// CHFRICH
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long int t;
+    int A, B, X;
+    cin>>t;
+    while(t--){
+        cin>>A>>B>>X;
+        cout<<(B-A)/X<<endl;
+    }
+	return 0;
+}
