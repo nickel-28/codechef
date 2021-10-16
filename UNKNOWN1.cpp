f69bdcf959a4efd643641b8c5e869404b8b1c317
nickel-28 <nikhilraj875727@gmail.com>
2021-10-16 11:25:32 +0530

    New Problem

---
+++ UNKNOWN1.cpp
@@ -0,0 +1,17 @@
+// UNKNOWN1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b;
+	cin >> a >> b;
+	
+	cout << fixed << setprecision(1) << (a + b) <<endl;
+	cout << fixed << setprecision(1) << (a - b) <<endl;
+	cout << fixed << setprecision(1) << (a * b) <<endl;
+	cout << fixed << setprecision(1) << (a / b) <<endl;
+	
+	return 0;
+}
+
