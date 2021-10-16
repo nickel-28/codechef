3077611ae0702d4f5018e6bc307f2dd3d0c15a23
nickel-28 <nikhilraj875727@gmail.com>
2021-10-16 11:18:38 +0530

    New problem

---
+++ FLOW002.cpp
@@ -0,0 +1,15 @@
+// /FLOW002
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t,A,B;
+    cin>>t;
+    
+    while(t--){
+        cin>>A>>B;
+        cout<<A%B<<endl;
+    }
+    return 0;
+}
