605d7f5fb45db2f3e308d37977293ef7080d4a5f
nickel-28 <nikhilraj875727@gmail.com>
2021-10-18 08:06:06 +0530

    New Problem

---
+++ FSQRT.cpp
@@ -0,0 +1,16 @@
+//  FSQRT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,r;
+	    cin>>n;
+	    r = sqrt(n);
+	    cout<<floor(r)<<endl;
+	}
+	return 0;
+}
