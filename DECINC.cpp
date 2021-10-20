5225d0ead57a80dd5fd0282d19a68d510fbad481
nickel-28 <nikhilraj875727@gmail.com>
2021-10-21 04:59:36 +0530

    New Problem

---
+++ DECINC.cpp
@@ -0,0 +1,26 @@
+// DECINC
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n%4==0){
+        n++;
+        cout << n <<endl;
+    }
+    else{
+        n--;
+        cout << n <<endl;
+    }
+    
+    
+}
+
+int main() {
+	
+	solve();
+	return 0;
+}
