1fad2adcdcdebea0c4750ded11b11f9ed2e28850
nickel-28 <nikhilraj875727@gmail.com>
2021-10-21 04:57:41 +0530

    New Problem

---
+++ CHOPRT.cpp
@@ -0,0 +1,24 @@
+// CHOPRT
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    if(a>b) cout<<">"<<endl;
+    else if(a<b) cout<<"<"<<endl;
+    if(a==b) cout<<"="<<endl;
+    
+    
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
