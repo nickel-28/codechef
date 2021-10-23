dbe15a9f1bd85245435482aab306712a28eec176
nickel-28 <nikhilraj875727@gmail.com>
2021-10-23 18:18:08 +0530

    New Problem Added

---
+++ ZCOSCH.cpp
@@ -0,0 +1,19 @@
+// ZCOSCH
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int r;
+    cin>>r;
+    if(r>=1 && r<=50) cout <<"100"<<endl;
+    else if(r>=51 && r<=100) cout <<"50"<<endl;
+    else if(r>100) cout <<"0"<<endl;
+    
+}
+
+int main() {
+	solve();
+	return 0;
+}
