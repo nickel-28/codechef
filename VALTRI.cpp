a02b4e2a6e5976ae18c76375c9561c38d487044b
nickel-28 <nikhilraj875727@gmail.com>
2021-11-09 18:11:38 +0530

    New Problem

---
+++ VALTRI.cpp
@@ -0,0 +1,19 @@
+// VALTRI
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    
+    if(n%5==0 || n%6==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    
+}
+
+int main() {
+	solve();
+	return 0;
+}
