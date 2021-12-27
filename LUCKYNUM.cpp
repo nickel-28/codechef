e9969189b826ede6d97dc26b0759acb63f7fada7
nickel-28 <nikhilraj875727@gmail.com>
2021-12-27 15:04:16 +0530

    New Problem

---
+++ LUCKYNUM.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    if(a==7 || b==7 || c==7){
+        cout<<"YES"<<endl;
+    }
+    else cout<<"NO"<<endl;
+    
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
