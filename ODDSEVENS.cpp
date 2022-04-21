f71bbf2162ac91c00223abab198d1f64c6c8e60e
nickel-28 <nikhilraj875727@gmail.com>
2022-04-21 11:00:08 +0530

    New Problem

---
+++ ODDSEVENS.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PI 3.1415926535897932384626
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    if((a+b)%2!=0) cout<<"Alice\n";
+    else cout<<"Bob\n";
+    
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
