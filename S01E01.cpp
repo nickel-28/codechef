5bdca381462f5843a19c7f2190b31198302f1a30
nickel-28 <nikhilraj875727@gmail.com>
2022-04-24 09:38:21 +0530

    New Problem

---
+++ S01E01.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PI 3.1415926535897932384626
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n>=21)
+        cout<<"YES\n";
+    else
+        cout<<"NO\n";
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	int t;
+	cin >> t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
